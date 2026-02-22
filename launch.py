01c76202d908133fd9cf6985e357615eb2fdeaee
Manasa Chittamanasareddy <chittamanasareddy@gmail.com>
2026-02-22 11:52:00 +0530

    Final submitted Ros assignment

---
+++ launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    talker_node = Node(
+        package='ros2_launch_demo',
+        executable='talker',
+        name='talker'
+    )
+
+    listener_node = Node(
+        package='ros2_launch_demo',
+        executable='listener',
+        name='listener'
+    )
+
+    return LaunchDescription([
+        talker_node,
+        listener_node
+    ])
